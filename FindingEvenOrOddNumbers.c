422bc70df87349809b8a2ac0141a662fc1a474eb
SATYODAY SHARMA <142677056+satyodaysharma@users.noreply.github.com>
2024-10-20 09:18:52 +0530

    Update FindingEvenOrOddNumbers.c

--- FindingEvenOrOddNumbers.c
+++ FindingEvenOrOddNumbers.c
@@ -1,1 +1,21 @@
+#include<stdio.h>
 
+int main()
+{
+    int a;
+
+    printf("Please enter a number to check if it is even or odd: ");
+    scanf("%d", &a);
+
+    if (a%2 == 0)
+    {
+        printf("%d is a even number", a);
+    }
+
+    else
+    {
+        printf("%d is a odd number", a);
+    }
+
+    return 0;
+}
