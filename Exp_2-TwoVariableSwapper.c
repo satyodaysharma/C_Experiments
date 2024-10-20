f70b60c4fbf47be4e10c3d8f996aae13164031ff
SATYODAY SHARMA <142677056+satyodaysharma@users.noreply.github.com>
2024-10-20 15:15:02 +0530

    Update Exp_2-TwoVariableSwapper.c

--- Exp_2-TwoVariableSwapper.c
+++ Exp_2-TwoVariableSwapper.c
@@ -4,19 +4,19 @@ int main()
 {
    float a, b, t;
 
-   printf("Please enter the First number: ");
+   printf("Please enter the First Number: ");
    scanf("%f", &a);
 
    printf("Please enter the Second Number: ");
    scanf("%f", &b);
 
-   printf("The values before swaping are: a = %.2f, b = %.2f", a, b);
+   printf("The values before swapping are: a = %.2f, b = %.2f", a, b);
 
    t = a;
    a = b;
    b = t;
 
-   printf("\nThe values after swaping are: a = %.2f, b = %.2f", a, b);
+   printf("\nThe values after swapping are: a = %.2f, b = %.2f", a, b);
 
    return 0;
 }
